bdeb3d73a9ff6271db943f48d2947dc27bc71efe
Andrew Kondelin <akondelin@ip-10-0-0-215.ap-southeast-1.compute.internal>
2019-11-06 20:05:29 +0700

    testing dependencies

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -x
 curl -sL https://rpm.nodesource.com/setup_10.x | sudo -E bash -
 sudo yum install -y gcc-c++ make git docker nodejs
-sudo npm install
 sudo npm install -g pm2
 sudo service docker start
 cd /home/ec2-user
+sudo npm install
 sudo docker stop mongo
 sudo docker rm -f mongo
 sudo docker stop app
